ebc3864e9e3d477e2226ef1fb3f1c008affda32a
Vaishnavip2706 <130354406+Vaishnavip2706@users.noreply.github.com>
2025-07-24 17:29:20 +0530

    NoteState.js

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import noteContext from "./noteContext";
 
 const NoteState = (props) => {
-  const host = "https://inotebook-xzn9.onrender.com";
+  const host = "http://localhost:5000";
   const notesInitial = [];
   const [notes, setNotes] = useState(notesInitial);
 

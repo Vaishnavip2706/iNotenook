34b41087ddd8451a37aa5f22bde8a12fee4ef8fc
Vaishnavip2706 <130354406+Vaishnavip2706@users.noreply.github.com>
2025-07-24 20:43:21 +0530

    db.js

--- backend/db.js
+++ backend/db.js
@@ -4,7 +4,11 @@ const mongoose =  require('mongoose')
 const mongoURI = "mongodb+srv://vaishnavipandey131:HMseAV7QLxqct35j@cluster0.yrev3gk.mongodb.net/"
 
 const connectToMongo = async ()=>{
-   await mongoose.connect(mongoURI);
+   await mongoose.connect(mongoURI {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      ssl: true, // important for MongoDB Atlas
+    });
         console.log("connected to mongo successfully")
 }
 
